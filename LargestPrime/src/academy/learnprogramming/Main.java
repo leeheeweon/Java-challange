8cb5626d616709be7eca0677b69ea9d9257a68c6
leeheeweon <ggoddii@kaoni.com>
2021-10-05 07:37:36 +0900

    LargestPrime

---
+++ LargestPrime/src/academy/learnprogramming/Main.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(getLargestPrime(45));
+    }
+
+
+    public static int getLargestPrime(int number) {
+        int i;
+        int copyOfInput = number;
+        if (number <= 1) {
+            return -1;
+        }
+        for (i = 2; i <= copyOfInput; i++) {
+            if (copyOfInput % i == 0) {
+                copyOfInput /= i;
+                i--;
+            }
+        }
+        return i;
+    }
+}
+
