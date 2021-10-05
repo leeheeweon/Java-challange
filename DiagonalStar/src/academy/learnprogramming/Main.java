2304e5c7561f940acb1156bc5968dff6153dc4ba
leeheeweon <ggoddii@kaoni.com>
2021-10-05 16:15:41 +0900

    DiagonalStar

---
+++ DiagonalStar/src/academy/learnprogramming/Main.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        printSquareStar(5);
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int i = 0; i < number; i++) {
+
+                for (int j = 0; j < number; j++) {
+
+                    if (i == 0 || j == 0 || i == j
+                            || i == number - 1 || j == number - 1
+                            || i + j == number - 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
