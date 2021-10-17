96b666f3856ac9f6d1c9c2e2c75fdee03f9ce835
leeheeweon <ggoddii@naver.com>
2021-10-17 15:08:14 +0900

    OOPTest2

--- OOPTest/src/com/company/Main.java
+++ OOPTest/src/com/company/Main.java
@@ -10,6 +10,14 @@ public class Main {
         hamburger.addHamburgerAddition1("Tomato", 0.27);
         hamburger.addHamburgerAddition2("Lettuce", 0.75);
         hamburger.addHamburgerAddition3("Cheese", 1.13);
-        System.out.println("Total burger price is" +hamburger.itemizeHamburger());
+        System.out.println("Total burger price is" + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHealthyAddition1("Egg", 5.43);
+        healthyBurger.addHealthyAddition2("Lentils", 3.41);
+        System.out.println("Total healthy burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        System.out.println("Total deluxe burger price is" + deluxeBurger.itemizeHamburger());
     }
 }
