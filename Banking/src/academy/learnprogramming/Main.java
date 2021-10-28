9e7b064dd64a5883dd7161a0a8f95a14e25a844f
leeheeweon <ggoddii@kaoni.com>
2021-10-29 07:49:49 +0900

    Banking 3

--- Banking/src/academy/learnprogramming/Main.java
+++ Banking/src/academy/learnprogramming/Main.java
@@ -3,6 +3,22 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Bank bank = new Bank("korea bank");
+        bank.addBranch("adelaide");
+        bank.addCustomer("adelaide", "Tim", 50.05);
+        bank.addCustomer("adelaide", "lee", 175.05);
+        bank.addCustomer("adelaide", "Thee", 220.05);
+
+        bank.addBranch("yongin");
+        bank.addCustomer("yongin", "lee2", 150.54);
+
+        bank.addCustomerTransaction("adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("adelaide", "lee", 12.22);
+        bank.addCustomerTransaction("adelaide", "Thee", 1.22);
+
+        bank.listCustomer("adelaide", true);
+        bank.listCustomer("yongin",true);
+
     }
 }
