9b9ca70d548df3103a7468101207113e2fbaf822
leeheeweon <ggoddii@kaoni.com>
2021-11-16 06:57:18 +0900

    AbstractClassesChallenge1

---
+++ AbstractClassesChallenge/src/academy/learnprogramming/NodeList.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public interface NodeList {
+    ListItem getRoot();
+
+    boolean addItem(ListItem item);
+
+    boolean removeItem(ListItem item);
+
+    void traverse(ListItem root);
+
+
+}
