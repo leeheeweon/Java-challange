1249140e8293a95ddbbdd5ec667bbe47b3876a09
leeheeweon <ggoddii@kaoni.com>
2021-11-17 06:56:42 +0900

    AbstractClassesChallenge2

--- AbstractClassesChallenge/src/academy/learnprogramming/Main.java
+++ AbstractClassesChallenge/src/academy/learnprogramming/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+
+        String[] data = stringData.split(" ");
+
+        for(String s : data) {
+            list.addItem(new Node(s));
+        }
+
+        list.traverse(list.getRoot());
     }
 }
