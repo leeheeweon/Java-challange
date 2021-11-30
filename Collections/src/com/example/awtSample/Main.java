a9e1d0c010c7dbda14b48996a7f9e48e6fe52cf8
leeheeweon <ggoddii@kaoni.com>
2021-12-01 06:48:48 +0900

    Collections List Method

--- Collections/src/com/example/awtSample/Main.java
+++ Collections/src/com/example/awtSample/Main.java
@@ -1,5 +1,7 @@
 package com.example.awtSample;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -8,18 +10,45 @@ public class Main {
         // write your code here
         Theater theater = new Theater("CGV", 8, 12);
 //        theater.getSeat();
+        List<Theater.Seat> seatCopy = new ArrayList<>(theater.seats);
+        printList(seatCopy);
 
+        seatCopy.get(1).reserve();
         if (theater.reserveSeat("H11")) {
             System.out.println("Please pay");
         } else {
             System.out.println("Sorry, seat is taken");
         }
+
+        Collections.reverse(seatCopy);
+        System.out.println("Printing seatCopy");
+        printList(seatCopy);
+        System.out.println("Printing theater.seat");
+        printList(theater.seats);
+
+        Theater.Seat minSeat = Collections.min(seatCopy);
+        Theater.Seat maxSeat = Collections.max(seatCopy);
+        System.out.println("Min seat number is " + minSeat.getSeatNumber());
+        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+
+        sortList(seatCopy);
+        System.out.println("Printing sorted seatCopy");
+        printList(seatCopy);
     }
 
     public static void printList(List<Theater.Seat> list) {
         for (Theater.seat :list){
             System.out.println(" " + seat.getSeatNumber());
         }
+    }
 
+    public static void sortList(List<? extends Theater.Seat> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
